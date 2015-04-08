1a54f8621b11c52f64c021284e1f4bae71b5e499
Artem Kostiuk <postatum@gmail.com>
2015-04-08 17:22:11 +0300

    A bit of code refactoring

--- example_api/model/user.py
+++ example_api/model/user.py
@@ -56,13 +56,10 @@ class User(BaseDocument):
         document='Story', ondelete='NULLIFY',
         backref_name='user', backref_ondelete='NULLIFY')
 
-    id = IdField(
-        primary_key=True
-    )
+    id = IdField(primary_key=True)
     timestamp = DateTimeField(default=datetime.utcnow)
 
     username = StringField(
-        # primary_key=True,
         min_length=1, max_length=50, unique=True,
         processors=[random_uuid, lower_strip])
     email = StringField(unique=True, required=True, processors=[lower_strip])
