cd42e52d21d3499cc50c0783ba65ea2df9f60495
Artem Kostiuk <postatum@gmail.com>
2015-08-28 16:26:58 +0300

    Use ACLView mixin instead of ACLview

--- example_api/views/stories.py
+++ example_api/views/stories.py
@@ -1,7 +1,7 @@
 import logging
 from random import random
 
-from nefertari.view import ACLFilterBaseView
+from nefertari.view import BaseView, ACLFilterViewMixin
 
 from example_api.models import Story
 
@@ -16,7 +16,7 @@ class ArbitraryObject(object):
         return dict(attr=self.attr)
 
 
-class StoriesView(ACLFilterBaseView):
+class StoriesView(ACLFilterViewMixin, BaseView):
     Model = Story
 
     def index(self):
